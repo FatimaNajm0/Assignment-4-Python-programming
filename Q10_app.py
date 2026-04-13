27ea2a31e3e37b6a49b166b3c2073009faff410e
FatimaNajm0 <s1024009@uomustansiriyah.edu.iq>
2026-04-13 23:23:57 +0300

    Create Q10_app.py

---
+++ Q10_app.py
@@ -0,0 +1,13 @@
+import Q10_shapes
+
+print("--- Circle ---")
+print("Area:", shapes.circle_area(5))
+print("Circumference:", shapes.circle_circumference(5))
+
+print("\n--- Square ---")
+print("Area:", shapes.square_area(4))
+print("Perimeter:", shapes.square_perimeter(4))
+
+print("\n--- Rectangle ---")
+print("Area:", shapes.rectangle_area(4, 6))
+print("Perimeter:", shapes.rectangle_perimeter(4, 6))
