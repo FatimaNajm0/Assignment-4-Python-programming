2957e49a5f55fb70e61e3e2724122b6b6760f401
FatimaNajm0 <s1024009@uomustansiriyah.edu.iq>
2026-04-13 23:22:55 +0300

    Create Q10_shapes.py

---
+++ Q10_shapes.py
@@ -0,0 +1,17 @@
+def circle_area(radius):
+    return 3.14 * radius * radius
+
+def circle_circumference(radius):
+    return 2 * 3.14 * radius
+
+def square_area(side):
+    return side * side
+
+def square_perimeter(side):
+    return 4 * side
+
+def rectangle_area(length, width):
+    return length * width
+
+def rectangle_perimeter(length, width):
+    return 2 * (length + width)
