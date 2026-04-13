905c3357c641aa2905ac7dda439f97712cbb7f4e
FatimaNajm0 <s1024009@uomustansiriyah.edu.iq>
2026-04-13 23:14:04 +0300

    Create Q7_app.py

---
+++ Q7_app.py
@@ -0,0 +1,7 @@
+import Q7_edit_string
+
+my_text = "Software Engineering"
+
+print(edit_string.inverse_string(my_text))
+print(edit_string.check_length(my_text))
+print(edit_string.delete_spaces(my_text))
